cc5cbb3dc8df378e867bd19b6d3b886d436e2673
Jae Jeong <jeongjj29@gmail.com>
2024-08-24 19:36:06 -0400

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    if i == 0:
+        print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    squared_integer_list = [int * int for int in int_list]
+    return squared_integer_list
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(100):
+        if (i + 1) % 3 == 0 and (i + 1) % 5 == 0:
+            print("FizzBuzz")
+        elif (i + 1)% 3 == 0:
+            print("Fizz")
+        elif (i + 1)% 5 == 0:
+            print("Buzz")
+        else:
+            print(i + 1)
     pass
